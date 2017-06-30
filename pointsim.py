958af9fd3df83f65e849342be852b9c14895d6d0
Thaddeus Hughes <hughes.thad@gmail.com>
2017-06-29 19:47:01 -0500

    Fixed skidpad scoring

--- pointsim.py
+++ pointsim.py
@@ -2,7 +2,7 @@ def compute_points(event_name, best_time, best_co2, times, co2s):
 	if event_name == 'acceleration':
 		return 95.5*(best_time*1.5/times - 1)/(1.5-1) + 4.5
 	if event_name == 'skidpad':
-		return 71.5*((best_time*1.45/times)^2 - 1)/(1.45^2-1) + 3.5
+		return 71.5*((best_time*1.45/times)**2 - 1)/(1.45**2-1) + 3.5
 	if event_name == 'autocross':
 		return 118.5*(best_time*1.45/times - 1)/(1.45-1) + 6.5
 	if event_name == 'endurance':
